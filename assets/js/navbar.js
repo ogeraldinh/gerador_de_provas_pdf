190560d4bba9b982d13a9d8f3dc8fd2ecc2bcb2d
Geraldo Duarte <geraldoduartedemedeirosneto@gmail.com>
2025-03-10 13:50:24 -0300

    footer-idea

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navbarHTML = `  
-    <nav class="navbar">
     <div id="logo">
       <img src="assets/img/logo.svg" alt="" />
     </div>
     <ul>
-      <li><a href="index.html" id="selected">Início</a></li>
-      <li><a href="professor.html">Professor</a></li>
-      <li><a href="questoes.html">Questões</a></li>
-      <li><a href="prova.html">Gerar Prova</a></li>
-    </ul>
-  </nav>`;
+      <li><a href="index.html" class="inicio-select">Início</a></li>
+      <li><a href="professor.html" class="professor-select">Professor</a></li>
+      <li><a href="questoes.html" class="questoes-select">Questões</a></li>
+      <li><a href="prova.html" class="prova-select">Gerar Prova</a></li>
+    </ul>`;
 
   document.querySelector(".navbar").innerHTML = navbarHTML
 });
