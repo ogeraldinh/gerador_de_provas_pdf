e510c0348aedc986d0c38188b1f18e26412b9da6
Geraldo Duarte <geraldoduartedemedeirosneto@gmail.com>
2025-03-10 20:48:03 -0300

    : C

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,33 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     const footerHTML = `  
     <div><img src="assets/img/icon2.svg" alt=""></div>
-    <div class="footer-content">
 
     <div class="footer-title">
-      <h2>Sobre</h2>
-      <h2>Sobre</h2>
-      <h2>Sobre</h2>
+      <h2>Desenvolvedores</h2>
+      <h2>Navegação</h2>
+      <h2>Contatos</h2>
     </div>
       
       <div class="footer-links">
         <ul>
-          <li><a href="#">teste</a></li>
-          <li><a href="#">teste</a></li>
-          <li><a href="#">teste</a></li>
+          <li><a href="#">Geraldo Duarte</a></li>
+          <li><a href="#">Lígia Nascimento</a></li>
+          <li><a href="#">Davi Freitas</a></li>
+          <li><a href="#">Paulo Flávio</a></li>
+          <li><a href="#">Márcio Gabriel</a></li>
         </ul>
         
         <ul>
-          <li><a href="#">teste</a></li>
-          <li><a href="#">teste</a></li>
-          <li><a href="#">teste</a></li>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Professor</a></li>
+          <li><a href="#">Questões</a></li>
         </ul>
         
         <ul>
-          <li><a href="#">teste</a></li>
-          <li><a href="#">teste</a></li>
-          <li><a href="#">teste</a></li>
+          <li>teste@gmail.com</li>
+          <li>+55 (85) 9 9999-9999</li>
         </ul>
-      </div>
 
     </div>`;
 
